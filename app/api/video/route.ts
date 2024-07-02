bbc6dc627c6ff10a364cca1316d8c0d6e407bf30
ginlord <nicandro.ronnie@gmail.com>
2024-07-02 22:46:42 +0800

    d

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -1,5 +1,5 @@
 import { checkApiLimit, increaseApiLimit } from "@/lib/api-limit";
-import { auth } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import Replicate from "replicate";
 const replicate = new Replicate({
